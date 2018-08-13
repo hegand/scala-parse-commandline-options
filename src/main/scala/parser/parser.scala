df3777cfba881d2394f8f6ca6cf20306416ceba0
András Hegedüs <ahegedus@hortonworks.com>
2018-08-13 09:58:25 +0200

    adding checkConfig and print job name

--- src/main/scala/parser/parser.scala
+++ src/main/scala/parser/parser.scala
@@ -1,12 +1,12 @@
 package parser
-import org.apache.log4j.{Level, LogManager}
+import org.apache.log4j.{Level, LogManager, Logger}
 
 import scala.io.Source
 import org.apache.spark.sql.SparkSession
 import scopt.OptionParser
 
 case class JobConfiguration(job: String = null,
-                            dates: String = null,
+                            days: String = null,
                             configPath: String = null)
 
 object Parser {
@@ -21,25 +21,34 @@ object Parser {
     opt[String]("days")
       .text("Date Range")
       .required()
-      .action((x, config) => config.copy(dates = x))
+      .action((x, config) => config.copy(days = x))
 
     opt[String]("config")
       .text("Config Path")
       .action((x, config) => config.copy(configPath = x))
 
     help("help").text("prints this help text")
+
+    checkConfig( c =>
+      if (c.days == None) {
+        failure("dates must be specified")
+      } else success
+    )
   }
 
   def main(args: Array[String]) {
     val log = LogManager.getRootLogger
     log.setLevel(Level.INFO)
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
 
     val spark = SparkSession.builder().getOrCreate()
     log.info("Spark version " + (spark.version))
 
     cmdLineParser.parse(args, JobConfiguration()) map { jobConfig =>
-      log.warn("Starting PARSER...")
-      log.warn(s"config = $jobConfig")
+      log.info("Starting PARSER...")
+      log.info(s"config = $jobConfig")
+      log.info("job = " + jobConfig.job)
 
       for (line <- Source.fromFile(jobConfig.configPath).getLines) {
         log.warn(line)
